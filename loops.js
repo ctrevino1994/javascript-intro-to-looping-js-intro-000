87fcd62a4635d3cf4fa88c1233ebb078eb1c5fbc
Christian Trevino <ctrevino1994@yahoo.com>
2020-02-22 03:34:40 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,3 +16,16 @@ function whileLoop(n) {
   }
   return 'done';
 }
+
+function doWhileLoop(num) {
+  var i = num;
+
+  function incrementalVariable() {
+    i = i + 1;
+    return i;
+  }
+  do {
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
+
+}
